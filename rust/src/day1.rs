d394f7ac67e55276f8a7d4745ca6346313c8358e
sebastian <sebastian@datavant.com>
2023-12-01 14:42:15 -0700

    cleanup

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use std::fs::read_to_string;
 
-pub fn get_digits(line: &str) -> Vec<u32> {
+fn get_digits(line: &str) -> Vec<u32> {
     let nums = vec![
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
@@ -23,14 +23,16 @@ pub fn get_digits(line: &str) -> Vec<u32> {
     result
 }
 
+fn calibrate(nums: Vec<u32>) -> u32 {
+    nums.first().expect("Num must exist") * 10 + nums.last().expect("Num must exist")
+}
+
 pub fn solve_b(filename: &str) -> Result<u32> {
     let text = read_to_string(filename)?;
     let lines = text.lines();
     let nums: Vec<_> = lines
         .map(|line| get_digits(line))
-        .map(|nums| {
-            nums.first().expect("Num must exist") * 10 + nums.last().expect("Num must exist")
-        })
+        .map(calibrate)
         .collect();
 
     Ok(nums.iter().sum())
@@ -46,9 +48,7 @@ pub fn solve_a(filename: &str) -> Result<u32> {
                 .map(|c| c.to_digit(10).unwrap())
                 .collect::<Vec<_>>()
         })
-        .map(|nums| {
-            nums.first().expect("Num must exist") * 10 + nums.last().expect("Num must exist")
-        })
+        .map(calibrate)
         .collect();
 
     Ok(nums.iter().sum())
