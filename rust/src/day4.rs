5410696c8b22eabe1d6a0c24cd5a530ab3ef2ae2
sebastian <sebastian@datavant.com>
2023-12-04 17:35:29 -0700

    changes

--- rust/src/day4.rs
+++ rust/src/day4.rs
@@ -4,7 +4,7 @@ use nom::{
     character::complete::{digit1, space0, space1},
     combinator::map_res,
     multi::separated_list0,
-    sequence::separated_pair,
+    sequence::{preceded, separated_pair, tuple},
     IResult,
 };
 use std::{collections::HashMap, str::FromStr};
@@ -14,31 +14,28 @@ pub struct Card {
     held: Vec<u32>,
 }
 
+impl Card {
+    fn overlap(self: &Self) -> usize {
+        self.held
+            .iter()
+            .filter(|x| self.winning.contains(x))
+            .count()
+    }
+}
+
 fn parse_number(input: &str) -> IResult<&str, u32> {
     map_res(digit1, |s: &str| u32::from_str(s))(input)
 }
 
 fn parse_vec(input: &str) -> IResult<&str, Vec<u32>> {
-    let (input, _) = space0(input)?;
-    let (input, parts) = separated_list0(space1, parse_number)(input)?;
-
-    Ok((input, parts))
+    preceded(space0, separated_list0(space1, parse_number))(input)
 }
 
 fn parse_card(input: &str) -> IResult<&str, Card> {
-    let (input, _) = is_not(":")(input)?;
-    let (input, _) = tag(":")(input)?;
-    let (input, _) = space0(input)?;
+    let (input, _) = tuple((is_not(":"), tag(":"), space0))(input)?;
     let (input, (winning, held)) = separated_pair(parse_vec, tag(" | "), parse_vec)(input)?;
 
     Ok((input, Card { winning, held }))
-}
-
-fn overlap(card: &Card) -> usize {
-    card.held
-        .iter()
-        .filter(|x| card.winning.contains(x))
-        .count()
 }
 
 #[aoc_generator(day4)]
@@ -53,7 +50,7 @@ pub fn generate_input(input: &str) -> Vec<Card> {
 pub fn solve_a(cards: &[Card]) -> u32 {
     cards
         .iter()
-        .map(|card| overlap(&card) as u32)
+        .map(|card| card.overlap() as u32)
         .filter(|x| x > &0)
         .map(|x| 2_u32.pow(x - 1))
         .sum()
@@ -64,8 +61,7 @@ pub fn solve_b(cards: &[Card]) -> u32 {
     let mut counts: HashMap<usize, u32> = HashMap::new();
     for (i, card) in cards.iter().enumerate() {
         let cnt = *counts.entry(i).or_insert(1);
-        let num = overlap(&card);
-        (i + 1..=i + num).for_each(|k| *counts.entry(k).or_insert(1) += cnt);
+        (i + 1..=i + card.overlap()).for_each(|k| *counts.entry(k).or_insert(1) += cnt);
     }
 
     counts.values().sum()
