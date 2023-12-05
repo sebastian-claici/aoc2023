db8d4529b0c36f438ea38569923273d15e348cb2
sebastian <sebastian@datavant.com>
2023-12-05 08:13:47 -0700

    aesthetics

--- rust/src/day5.rs
+++ rust/src/day5.rs
@@ -1,15 +1,14 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{digit1, multispace1, space0, space1},
-    combinator::map_res,
-    multi::separated_list0,
-    sequence::{preceded, terminated},
+    character::complete::{line_ending, multispace1, space0, space1, u64},
+    multi::{many1, separated_list0},
+    sequence::{preceded, terminated, tuple},
     IResult,
 };
 use rayon::prelude::*;
-use std::str::FromStr;
 
 #[derive(Debug)]
 pub struct Data {
@@ -17,20 +16,23 @@ pub struct Data {
     maps: Vec<Vec<(u64, u64, u64)>>,
 }
 
-fn parse_number(input: &str) -> IResult<&str, u64> {
-    map_res(digit1, |s: &str| u64::from_str(s))(input)
-}
-
 fn parse_vec(input: &str) -> IResult<&str, Vec<u64>> {
-    preceded(space0, separated_list0(space1, parse_number))(input)
+    preceded(space0, separated_list0(space1, u64))(input)
 }
 
-fn parse_map(input: &str) -> IResult<&str, Vec<Vec<u64>>> {
-    separated_list0(multispace1, separated_list0(space1, parse_number))(input)
+fn parse_tup(input: &str) -> IResult<&str, (u64, u64, u64)> {
+    let (input, (x, _, y, _, z)) = tuple((u64, space1, u64, space1, u64))(input)?;
+    Ok((input, (x, y, z)))
+}
+
+fn parse_map(input: &str) -> IResult<&str, Vec<(u64, u64, u64)>> {
+    let (input, _) = parse_map_tag(input)?;
+    many1(terminated(parse_tup, line_ending))(input)
 }
 
 fn parse_map_tag(input: &str) -> IResult<&str, &str> {
     alt((
+        tag("seed-to-soil map:\n"),
         tag("soil-to-fertilizer map:\n"),
         tag("fertilizer-to-water map:\n"),
         tag("water-to-light map:\n"),
@@ -42,18 +44,8 @@ fn parse_map_tag(input: &str) -> IResult<&str, &str> {
 
 fn parse_input(input: &str) -> IResult<&str, Data> {
     let (input, seeds) = preceded(tag("seeds: "), parse_vec)(input)?;
-    let (input, _) = terminated(multispace1, tag("seed-to-soil map:\n"))(input)?;
-    let (input, tmp_maps) = separated_list0(parse_map_tag, parse_map)(input)?;
+    let (input, maps) = many1(preceded(multispace1, parse_map))(input)?;
 
-    let mut maps = vec![];
-    for map in tmp_maps.iter() {
-        maps.push(
-            map.iter()
-                .filter(|v| !v.is_empty())
-                .map(|v| (v[0], v[1], v[2]))
-                .collect(),
-        );
-    }
     Ok((input, Data { seeds, maps }))
 }
 
@@ -62,24 +54,26 @@ pub fn generate_input(input: &str) -> Data {
     parse_input(input).unwrap().1
 }
 
-fn apply_r(maps: &Vec<Vec<(u64, u64, u64)>>, seed: u64) -> u64 {
+fn apply_map(map: &[(u64, u64, u64)], seed: u64) -> u64 {
     let mut result = seed;
-    for map in maps {
-        for (dst, src, len) in map.iter() {
-            if src <= &result && result < src + len {
-                result = dst + result - src;
-                break;
-            }
+    for (dst, src, len) in map.iter() {
+        if src <= &result && result < src + len {
+            result = dst + result - src;
+            break;
         }
     }
     result
+}
+
+fn apply_maps(maps: &Vec<Vec<(u64, u64, u64)>>, seed: u64) -> u64 {
+    maps.iter().fold(seed, |acc, map| apply_map(map, acc))
 }
 
 #[aoc(day5, part1)]
 pub fn solve_a(data: &Data) -> u64 {
     data.seeds
         .iter()
-        .map(|s| apply_r(&data.maps, *s))
+        .map(|s| apply_maps(&data.maps, *s))
         .min()
         .unwrap()
 }
@@ -94,7 +88,7 @@ pub fn solve_b(data: &Data) -> u64 {
 
     seeds
         .into_par_iter()
-        .map(|(st, en)| (st..=en).map(|x| apply_r(&data.maps, x)).min().unwrap())
+        .map(|(st, en)| (st..=en).fold(u64::MAX, |acc, val| acc.min(apply_maps(&data.maps, val))))
         .min()
         .unwrap()
 }
