872cfcd27614e9299b2d2aafc9f5e8f65e595629
sebastian <sebastian@datavant.com>
2023-12-17 07:53:12 -0700

    cleanup

--- python/day17.py
+++ python/day17.py
@@ -11,30 +11,26 @@ def dijkstra(costs, start, part2):
     r, c = start
     queue = [(0, (r, c, 0, 1, 0)), (0, (r, c, 1, 0, 0))]
     heapq.heapify(queue)
-    dists = {}
+    dists = {(r, c, 0, 1, 0): 0, (r, c, 1, 0, 0): 0}
 
     while queue:
         cst, (r, c, dr, dc, num) = heapq.heappop(queue)
-        if (r, c, dr, dc, num) in dists:
-            continue
-        dists[(r, c, dr, dc, num)] = cst
 
         moves = [[dr, dc], ROT_R @ [dr, dc], ROT_L @ [dr, dc]]
         for (ndr, ndc) in moves:
-            if dr == ndr and dc == ndc:
-                cnt = num + 1
-            else:
-                cnt = 1
-
+            cnt = num + 1 if (dr, dc) == (ndr, ndc) else 1
             if part2:
                 is_good = (cnt <= 10) and ((dr, dc) == (ndr, ndc) or num >= 4)
             else:
                 is_good = cnt <= 3
 
-            if 0 <= r + ndr < R and 0 <= c + ndc < C and is_good:
-                state = (r + ndr, c + ndc, ndr, ndc, cnt)
+            if not (0 <= r + ndr < R and 0 <= c + ndc < C and is_good):
+                continue
 
-                curr = cst + costs[r + ndr][c + ndc]
+            state = (r + ndr, c + ndc, ndr, ndc, cnt)
+            curr = cst + costs[r + ndr][c + ndc]
+            if curr < dists.get(state, float("inf")):
+                dists[state] = curr
                 heapq.heappush(queue, (curr, state))
 
     return dists
@@ -57,5 +53,3 @@ for (r, c, _, _, cnt), v in dists.items():
     if r == last[0] and c == last[1] and cnt >= 4 and v <= ans:
         ans = v
 print(ans)
-
-
